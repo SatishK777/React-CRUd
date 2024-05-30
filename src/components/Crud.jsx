b981e4244fa1e38e5e56ab11fd2269abaa7753cc
SatishK777 <144874624+SatishK777@users.noreply.github.com>
2024-05-30 23:08:51 +0530

    Update Crud.jsx

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Container from 'react-bootstrap/Container';
-// import { Alert } from 'bootstrap';
+import { Alert } from 'bootstrap';
 import generateUniqueId from 'generate-unique-id';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 
-function crud({ onSubmit }) {
+
+function Emp({ onSubmit }) {
 
     let key = 0;
 
     const [inputState, setInputState] = useState({
-        pname: '',
-        pprice: '',
-        pcat: '',
-        pmfg: '',
-        paddr: '',
+        fname: '',
+        num: '',
+        lname: '',
+        email: '',
+        add: '',
         // id:'',
     });
 
@@ -36,46 +39,83 @@ function crud({ onSubmit }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (inputState.id) {
+            setViewData(viewData.map((record) => {
 
-            // if(inputState.pname && inputState.pprice && inputState.pcat && inputState.pmfg && inputState.paddr){
-
-            const obj = {
-                id: generateUniqueId({
-                    length: 4,
-                    useLetters: false
-                }),
-                ...inputState
-            }
-            setViewData([...viewData, obj])
-
-            // console.log("Data : ", obj);
-
-
-            setInputState(
-                {
-                    pname: '',
-                    pprice: '',
-                    pcat: '',
-                    pmfg: '',
-                    paddr: '',
-                    // id:''
+                if (record.id === inputState.id) {
+                    
+                    setInputState(
+                        {
+                            fname: '',
+                            num: '',
+                            lname: '',
+                            email: '',
+                            add: '',
+                            // id:''
+                        }
+                    )
+                    return { ...record, ...inputState }
                 }
-            )
-            // }else{
-            //     alert("Please Fill All Fields....")
-            // }
+                else {
+                    return record
+                }
+                
+            }))
+        }else{
+               // if(inputState.fname && inputState.num && inputState.lname && inputState.email && inputState.add){
+
+        const obj = {
+            id: generateUniqueId({
+                length: 4,
+                useLetters: false
+            }),
+            ...inputState
+        }
+        setViewData([...viewData, obj])
+
+        // console.log("Data : ", obj);
+
+
+        setInputState(
+            {
+                fname: '',
+                num: '',
+                lname: '',
+                email: '',
+                add: '',
+                // id:''
+            }
+        )
+        // }else{
+        //     alert("Please Fill All Fields....")
+        // }
 
     }
+        }
+
+       
+
+    // const handleEdit = (id) => {
+    //     let rec = viewData;
+    //     let updateRec = rec.find((data) => {
+    //         return (
+    //             data.id === id
+    //         )
+    //     })
+    //     setInputState({ ...updateRec })
+    // };
 
     const handleEdit = (id) => {
-        let rec = viewData;
-        let updateRec = rec.find((data) => {
+
+        let record = viewData;
+        let updatedRecord = record.filter((data) => {
             return (
                 data.id === id
             )
         })
-        setInputState({ ...updateRec })
-    };
+
+        setInputState(updatedRecord[0]);
+    }
 
 
 
@@ -91,149 +131,161 @@ function crud({ onSubmit }) {
 
 
     return (
+        <>
+            <Container className='my-5 col-6 formmm'>
 
-        <Container >
-            <h1 className='py-5 text-center'>
-                PRODUCT DATA
-            </h1>
-            <Form className='border p-5 border-5 rounded bg-dark' onSubmit={handleSubmit}>
-                <Row className="mb-3">
+                <h2 className=' text-light p-2 new'>New Employee</h2>
+
+                <Form className='p-5' onSubmit={handleSubmit}>
+
                     <Form.Group as={Col}>
-                        <Form.Label className='text-light'>Product Name</Form.Label>
-                        <Form.Control type="name" placeholder="Enter your product" name='pname' value={inputState.pname} onChange={handleInput} />
+                        <Form.Label >First Name</Form.Label>
+                        <Form.Control type="name"  name='fname' value={inputState.fname} onChange={handleInput} />
                     </Form.Group>
 
                     <Form.Group as={Col}>
-                        <Form.Label className='text-light'>Price</Form.Label>
-                        <Form.Control type="number" placeholder="Enter Price" name='pprice' value={inputState.pprice} onChange={handleInput} />
+                        <Form.Label >Last Name</Form.Label>
+                        <Form.Control type="name"  name='lname' value={inputState.lname} onChange={handleInput} />
                     </Form.Group>
-                </Row>
-
-                <Row className="mb-3">
-                    <Form.Group as={Col}>
-                        <Form.Label className='text-light'>Catagory</Form.Label>
-                        <FloatingLabel name='pcat' onChange={handleInput}>
-                            <Form.Select aria-label="Floating label select example" name='pcat' onChange={handleInput} value={inputState.pcat}>
-                                <option>Choose Product catagory</option>
-                                <option>Fashion</option>
-                                <option>Cosmetics</option>
-                                <option>Gadjets</option>
-                            </Form.Select>
-                        </FloatingLabel>
-                    </Form.Group>
-                    {/* <Form.Group as={Col}>
-                        <Form.Label className='text-light'>Catagory</Form.Label>
-                        <Form.Control type="name" placeholder="Enter Product Catagory" name='pcat' value={inputState.pcat} onChange={handleInput} />
-                    </Form.Group> */}
 
                     <Form.Group as={Col}>
-                        <Form.Label className='text-light'>Company</Form.Label>
-                        <Form.Control type="name" placeholder="Manufacturer name" name='pmfg' value={inputState.pmfg} onChange={handleInput} />
+                        <Form.Label >Email</Form.Label>
+                        <Form.Control type="Email"  name='email' value={inputState.email} onChange={handleInput} />
                     </Form.Group>
-                </Row>
 
-                <Form.Group className="mb-3" controlId="formGridAddress1">
-                    <Form.Label className='text-light'>Address</Form.Label>
-                    <Form.Control type='text' placeholder="1234 Main St" name='paddr' value={inputState.paddr} onChange={handleInput} />
-                </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGridAddress1">
+                        <Form.Label >Address</Form.Label>
+                        <Form.Control type='text'  name='add' value={inputState.add} onChange={handleInput} />
+                    </Form.Group>
 
-                <Form.Label className='text-light'>ID</Form.Label>
-                <Form.Control type="name" name='id' value={inputState.id} />
+                    <Form.Group as={Col}>
+                        <Form.Label >Phone</Form.Label>
+                        <Form.Control type="number"  name='num' value={inputState.num} onChange={handleInput} />
+                    </Form.Group>
 
 
-                <Button variant="primary" type="submit" className='text-center mt-4 btn btn-success'>
-                    Submit
-                </Button>
-            </Form>
-            <table border='1' width='1300' align='center' className='mt-5 border border-5 rounded' >
-                <thead >
-                    <tr>
-                        <th>
-                            Sr.
-                        </th>
-                        <th>
-                            Product ID
-                        </th>
-                        <th>
-                            Product Name
-                        </th>
-                        <th>
-                            Price
-                        </th>
-                        <th>
-                            Catagory
-                        </th>
-                        <th>
-                            MFG
-                        </th>
-                        <th>
-                            Address
-                        </th>
-                        <th>
-                            Action
-                        </th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        viewData.map((data, index) => {
-                            return (
-                                <tr key={key++}>
-                                    <td>
-                                        {
-                                            index + 1
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.id
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.pname
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.pprice
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.pcat
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.pmfg
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            data.paddr
-                                        }
-                                    </td>
-                                    <td>
-                                        {
-                                            <>
-                                                <Button className='btn btn-primary rounded me-2' onClick={() => handleEdit(data.id)}>Edit</Button>
-                                                <Button className='btn btn-danger rounded' onClick={() => handleDelete(data.id)}>Delete</Button>
-                                            </>
-                                        }
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </Container>
+
+
+
+
+
+
+
+
+
+                    <Form.Control type="name" name='id' value={inputState.id} hidden />
+
+
+                    <Button variant="primary" type="submit" className='text-center mt-4 btn btn-success'>
+                        Submit
+                    </Button>
+                </Form>
+
+
+            </Container>
+            <Container>
+                <div>
+                    <h2 className='manage p-2 text-light'>
+                        Manage Employees
+                    </h2>
+                </div>
+                <table border='1' width='1400' align='center' className='mt-5 border border-5 rounded' >
+                    <thead >
+                        <tr>
+                            <th>
+                                Sr.
+                            </th>
+                            <th>
+                                First Name
+                            </th>
+                            <th>
+                                Last Name
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                            <th>
+                                Address
+                            </th>
+                            <th>
+                                Phone
+                            </th>
+
+                            <th>
+                                Action
+                            </th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            viewData.map((data, index) => {
+                                return (
+                                    <tr key={key++}>
+                                        <td>
+                                            {
+                                                index + 1
+                                            }
+                                        </td>
+
+                                        <td>
+                                            {
+                                                data.fname
+                                            }
+                                        </td>
+
+                                        <td>
+                                            {
+                                                data.lname
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                data.email
+                                            }
+                                        </td>
+
+
+                                        <td>
+                                            {
+                                                data.add
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                data.num
+                                            }
+                                        </td>
+                                        <td>
+                                            {
+                                                <>
+                                                <Button className='bg-light border border-light text-primary me-2'>
+                                                    <FontAwesomeIcon icon={faEye} />
+                                                    </Button>
+                                                    <Button className='bg-light border border-light text-warning me-2' onClick={() => handleEdit(data.id)}>
+                                                    <FontAwesomeIcon icon={faPenToSquare} />
+                                                    </Button>
+                                                    <Button className='bg-light border border-light me-2 text-danger' onClick={() => handleDelete(data.id)}>
+                                                    <FontAwesomeIcon icon={faTrash} />
+                                                    </Button>
+                                                </>
+                                            }
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+
+            </Container>
+        </>
+
+
 
 
     )
 }
 
-
-export default Crud
+export default Emp
